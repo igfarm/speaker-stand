0acce8ced2464390a4082a160f0db297d1c91cba
Jaime Pereira <jaime@iguana-farm.com>
2024-01-30 19:17:36 -0500

    updates

--- build_examples.sh
+++ build_examples.sh
@@ -1,59 +1,43 @@
 #!/bin/bash
 OPENSCAD="/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD --enable=manifold"
 
-set -e
-
 # Generate stands
-INFILE=../../tripod.scad
-for HEIGHT in 28 24 20
+INFILE=./tripod.scad
+for HEIGHT in 29
 do
     echo "Processing ${HEIGHT}"
     HEIGHT_MM=`bc -l <<< $HEIGHT*25.4`
-    pushd examples/${HEIGHT}-inch
+    ODIR="examples/${HEIGHT}-inch"
 
-    # Check if something has changed
-    rm -rf tmp
-    mkdir tmp
+    if [ ! -d $ODIR ]; then
+        echo creating dir
+        mkdir $ODIR
+    fi
 
-    $OPENSCAD -o tmp/tripod-tmp.stl \
-        -D 'stand_height='${HEIGHT_MM} \
+    # CMD="${OPENSCAD} -D 'default_height=${HEIGHT_MM}'";
+    CMD="${OPENSCAD} -D default_height=${HEIGHT_MM}"
+
+    
+    # There are changes, generate files again
+    $CMD -o $ODIR/tripod.stl \
+        $INFILE 2> $ODIR/tube_sizes.txt
+
+    $CMD -o $ODIR/tripod.png \
         $INFILE
 
-    echo 'difference(){import("../tripod.stl");import("tripod-tmp.stl");}' > tmp/diff.scad
-    $OPENSCAD -o tmp/result.stl \
-        -D 'stand_height='${HEIGHT_MM} \
-        tmp/diff.scad > tmp/diff.txt 2>&1  || true
+    $CMD -o $ODIR/tripod-hi.stl \
+        -D 'default_show="hi"'  \
+        $INFILE 
 
-    if grep -q "Current top level object is empty." tmp/diff.txt; then
-        echo "No changes"
-    else
-        # There are changes, generate files again
-        $OPENSCAD -o tripod.stl \
-            -D 'stand_height='${HEIGHT_MM} \
-            $INFILE 2> tube_sizes.txt
+    $CMD -o $ODIR/tripod-low.stl \
+        -D 'default_show="low"'  \
+        $INFILE
 
-        $OPENSCAD -o tripod.png \
-            -D 'stand_height='${HEIGHT_MM} \
-            $INFILE
+    $CMD -o $ODIR/tripod-platform.stl \
+        -D 'default_show="plate"'  \
+        $INFILE
 
-        $OPENSCAD -o $ODIR/tripod-hi.stl \
-            -D 'stand_height='${HEIGHT_MM} \
-            -D 'show_part=true' \
-            -D 'part="hi"'  \
-            $INFILE 
-
-        $OPENSCAD -o $ODIR/tripod-low.stl \
-        -D 'stand_height='${HEIGHT_MM} \
-            -D 'show_part=true' \
-            -D 'part="low"'  \
-            $INFILE
-
-        $OPENSCAD -o $ODIR/tripod-platform.stl \
-            -D 'stand_height='${HEIGHT_MM} \
-            -D 'show_part=true' \
-            -D 'part="platform"'  \
-            $INFILE
-    fi
-    rm -rf tmp
-    popd
+    $CMD -o $ODIR/tripod-test.stl \
+        -D 'default_show="test"'  \
+        $INFILE
 done
